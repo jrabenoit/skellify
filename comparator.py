774e522595ebfdf6817da93fbdd5083908b7458e
James Benoit <jrabenoit@gmail.com>
2016-08-02 14:56:44 -0600

    fixed train/test results

--- comparator.py
+++ comparator.py
@@ -55,18 +55,18 @@ def PickBest(test_results):
 
 def PrintFinal(final_train_results, final_test_results, n_1, n_2, final_train_predictions, final_test_predictions, final_train_labels, final_test_labels, iter_n, train_index_files, test_index_files):   
 
-    pprint(final_train_results)    
+#    pprint(final_train_results)    
     final_average_train = round(sum(final_train_results.values())/5, 3)
-    print('\n>>> Average of best tool across training sets: {}% <<<\n'.format(round(final_average_train, 2)))
+#    print('\n>>> Average of best tool across training sets: {}% <<<\n'.format(round(final_average_train, 2)))
 
-    pprint(final_test_results)
+#    pprint(final_test_results)
     final_average_test = round(sum(final_test_results.values())/5, 3)
-    print('\n>>> Average Tested Accuracy: {}% <<<\n'.format(round(final_average_test, 3)))
+#    print('\n>>> Average Tested Accuracy: {}% <<<\n'.format(round(final_average_test, 3)))
     
     n_max = max(n_1, n_2)
     n_min = min(n_1, n_2)
     expected_accuracy = (n_max/(n_min + n_max))*100
-    print('>>> Chance Accuracy For n_1= {} and n_2= {}: {}% <<<\n'.format(n_1, n_2, round(expected_accuracy, 3)))
+#    print('>>> Chance Accuracy For n_1= {} and n_2= {}: {}% <<<\n'.format(n_1, n_2, round(expected_accuracy, 3)))
     acc_diff = final_average_test - expected_accuracy
     if acc_diff > 0:
         acc_direction = str('above')
@@ -74,7 +74,7 @@ def PrintFinal(final_train_results, final_test_results, n_1, n_2, final_train_pr
         acc_direction = str('below')
     else: 
         acc_direction = str('equals')
-    print('>>> Tested accuracy {} chance accuracy by {}% <<<\n'.format(acc_direction, round(abs(acc_diff), 3)))
+#    print('>>> Tested accuracy {} chance accuracy by {}% <<<\n'.format(acc_direction, round(abs(acc_diff), 3)))
     
 # Create results dict
     final_test_correct = defaultdict(list)
@@ -120,8 +120,8 @@ def SubjectAccuracy(iter_n, final_train_correct, final_test_correct, train_index
     for i in range(iter_n):
         for j in range(5):
             for k in range(len(final_train_correct[i][j])):
-                list_of_test_results.append(final_train_correct[i][j][k])
-                list_of_test_subjects.append(train_index_files[i][j][k])
+                list_of_train_results.append(final_train_correct[i][j][k])
+                list_of_train_subjects.append(train_index_files[i][j][k])
     
     subject_results_test= defaultdict(list)
     for subj,result in zip(list_of_test_subjects,list_of_test_results):
@@ -130,13 +130,12 @@ def SubjectAccuracy(iter_n, final_train_correct, final_test_correct, train_index
         else:
             subject_results_test[subj] = [result]
 
-    subject_results_train= defaultdict(list)
+    subject_results_train= defaultdict(list)   
     for subj,result in zip(list_of_train_subjects,list_of_train_results):
         if subj in subject_results_train:
             subject_results_train[subj] += [result]
         else:
             subject_results_train[subj] = [result]
-
     return subject_results_test, subject_results_train
     
 
--- skellify.py
+++ skellify.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python 
 
-#Temporary test message for git setup
-
 #Data prep modules, processing modules, and result reporting modules
 from collections import defaultdict
 import data, prep, crossval, pprint, itertools
@@ -9,68 +7,77 @@ import iterator, comparator
 import visualize
 
 #Select a group of scans to use 
-print('Running Step 1/10: Select Groups for Classification')
+print('Step 1/10: Select Two Groups')
 n_1, n_2, dir_1, dir_2, mask = data.SelectGroup()
 
 concatenated_test = defaultdict(list)
 concatenated_train = defaultdict(list)
 
-for i in range(2):
-    print('>>> ITERATION {} OF 10'.format(i+1))  
-    print('Running Step 2/10: Sort & Mask Data')
+number_of_iterations = 10
+for i in range(number_of_iterations):
+    print('>>>ITERATION {} OF {}'.format(i+1,number_of_iterations))  
+    print('Step 2/10: Sort Data')
 #Load two sets of scans into a dataset and mask the data
     concat_dict, concat_subjects_dict, iter_n = prep.Sort(dir_1, dir_2, n_1, n_2)
 #Flatten the data, z-normalize (center the data (remove the mean), scale to unit variance)
-    print('Running Step 3/10: Flatten the 4D Files to 2D Matrices, Z-normalization')
+    print('Step 3/10: Flatten 4D Images to 2D Matrices, Z-normalization')
     masked_dict = prep.MaskFlatten(concat_dict, mask, iter_n)
     znorm_dict = prep.ZNormalize(masked_dict, iter_n)
-    print('Running Step 4/10: Label Groups')
+    print('Step 4/10: Label Groups')
     group_label_dict = prep.GroupLabels(n_1, n_2, iter_n)
 #Do 5-fold CV setup for outer fold (X=data, y=labels)
 #NOTE: THIS STEP AND THE NEXT STEP DO NOT RUN THE CV. THESE STEPS SET UP 25 DIFFERENT GROUPS OF DATA, AS DETERMINED BY A 5 OUTER FOLD CV, AND A 5 INNER-FOLD CV ON EACH OUTER FOLD.
-    print('Running Step 5/10: Set Up Outer CV Loop')
+    print('Step 5/10: Set Up Outer CV')
     oX_train, oX_test, oy_train, oy_test, train_index_outer, test_index_outer, train_index_files, test_index_files = crossval.oSkfCv(group_label_dict, znorm_dict, iter_n, concat_subjects_dict)
 #Do 5-fold CV setup for each outer fold, creating 25 inner folds total
-    print('Running Step 6/10: Set Up Inner CV Loop')
+    print('Step 6/10: Set Up Inner CV')
     iX_train, iX_test, iy_train, iy_test, train_index_inner, test_index_inner = crossval.iSkfCv(oy_train, oX_train, iter_n)
 #Run the data from iSkfCv through an iterative tool that tries all learning combinations
-    print('Running Step 7/10: List featsel/decomp/mltool Combos')
+    print('Step 7/10: Try all featsel/decomp/mltool Combos')
     test_results, param_set_list = iterator.ParameterSets(iX_train, iX_test, iy_train, iy_test, iter_n)
 #Compare all accuracy results to pick best combination of parameters
-    print('Running Step 8/10: Pick Best featsel/decomp/mltool Combo')
+    print('Step 8/10: Pick Best featsel/decomp/mltool Combo')
     fold_index, folds = comparator.PickBest(test_results)
-    print('Running Step 9/10: Run Winning Combo on Outer Loop Holdout')
+    print('Step 9/10: Run Best Combo on Outer CV Holdout')
 #Run best param set for each inner fold on corresponding outer fold
     final_train_results, final_test_results, final_train_predictions, final_test_predictions, final_train_labels, final_test_labels = iterator.TestHoldout(oX_train, oX_test, oy_train, oy_test, fold_index, iter_n) 
-    print('Running Step 10/10: Print Test vs. Chance Results')
+    print('Step 10/10: Print Test vs. Chance Results')
 #Print the results
     final_train_correct, final_test_correct = comparator.PrintFinal(final_train_results, final_test_results, n_1, n_2, final_train_predictions, final_test_predictions, final_train_labels, final_test_labels, iter_n, train_index_files, test_index_files)
 
-#Build bootstrap distribution
+#Build accuracy profile for each subject
     subject_results_test, subject_results_train = comparator.SubjectAccuracy(iter_n, final_train_correct, final_test_correct, train_index_files, test_index_files, concat_subjects_dict)
 
 #Concatenate classification attempts for each subject
     for key, value in subject_results_test.items():
         concatenated_test[key].append(value)
-    print(concatenated_test)
+
+    for key, value in subject_results_train.items():
+        concatenated_train[key].append(value)
+
+#print('>>>RAW TRAIN CLASSIFICATIONS')
+#print(concatenated_train)
+
+#print('>>>RAW TEST CLASSIFICATIONS')
+#print(concatenated_test)
 
 print('>>>CHAINING TRAINING RESULTS TOGETHER')
 concatenated_train_chained = defaultdict(list)
 for key, value in concatenated_train.items():
     concatenated_train_chained[key] = list(itertools.chain.from_iterable(value))
     
-print('>>>CHAINING TEST RESULTS TOGETHER')
+#print('>>>CHAINING TEST RESULTS TOGETHER')
 concatenated_test_chained = defaultdict(list)
 for key, value in concatenated_test.items():
     concatenated_test_chained[key] = list(itertools.chain.from_iterable(value))
 
-print('>>>TEST SUBJECT ACCURACY SCORES')
+print('>>>TRAIN SUBJECT ACCURACY SCORES')
 per_subject_train_acc = defaultdict(list)
 for key, value in concatenated_train_chained.items():
     per_subject_train_acc[key] = round((sum(value)/len(value))*100,2)
 pprint.pprint(per_subject_train_acc)
 
-print('>>>TRAIN SUBJECT ACCURACY SCORES')
+print('>>>TEST SUBJECT ACCURACY SCORES')
 per_subject_test_acc = defaultdict(list)
 for key, value in concatenated_test_chained.items():
     per_subject_test_acc[key] = round((sum(value)/len(value))*100,2)
